bf3c1d13f3c0459e42a05353f0aac102d15ae336
Zoli Kahan <zolikahan@gmail.com>
2013-05-27 02:50:28 -0500

    now using mongojs

--- feeder.js
+++ feeder.js
@@ -2,65 +2,15 @@
 
 console.log('feeder starting')
 
-var MongoClient = require('mongodb').MongoClient;
-var Q = require('q')
-
-var Feed = Q.defer()
-var FeedItem = Q.defer()
-var Subscription = Q.defer()
-
-MongoClient.connect('mongodb://localhost/retinus', function (err, db) {
-    if (!err && db) {
-        db.collection('feed', function (err, collection) {
-            if (err || !collection) {
-                console.error("failed to load Feed collection")
-                Feed.reject(err)
-            } else {
-                Feed.resolve(collection)
-            }
-        })
-        db.collection('feedItem', function (err, collection) {
-            if (err || !collection) {
-                console.error("failed to load FeedItem collection")
-                FeedItem.reject(err)
-            } else {
-                FeedItem.resolve(collection)
-            }
-        })
-        db.collection('subscription', function (err, collection) {
-            if (err || !collection) {
-                console.error("failed to load Subscription collection")
-                Subscription.reject(err)
-            } else {
-                Subscription.resolve(collection)
-            }
-        })
-    } else {
-        console.error("failed to load database")
-    }
-})
+var databaseUrl = "retinus"; // "username:password@example.com/mydb"
+var collections = ["feed", "feedItem", "subscription"]
+var db = require("mongojs")(databaseUrl, collections);
 
 var interval = 1000 * 60 * 20 //20 mins
-var Feed, FeedItem, Subscription
-Q.all([
-    Feed.promise,
-    FeedItem.promise,
-    Subscription.promise
-]).spread(function(feed, feedItem,subscription){
-    Feed = feed
-    FeedItem = feedItem
-    Subscription = subscription
-    updateAll()
-})
+
 function updateAll() {
-    Feed.findOne({"feedurl" : "http://feeds.arstechnica.com/arstechnica/index/"},function (err, feeds) {
-        console.log(feeds)
-    })
-    FeedItem.find().toArray(function (err, feeds) {
-        console.log(feeds)
-    })
-    Subscription.find().toArray(function (err, feeds) {
-        console.log(feeds)
+    db.feed.find(function(err, docs) {
+        console.log(docs)
     })
     /*
     newStuff = {}
@@ -84,5 +34,5 @@ for each user:
 
     setTimeout(this, interval)
 }
-
+updateAll()
 setTimeout(updateAll, interval)
